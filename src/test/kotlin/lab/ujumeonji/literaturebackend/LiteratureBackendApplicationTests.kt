823762d2c5a6378f12ec59d8d7e63d06861c4823
Byeongju, Shin <404err@naver.com>
2025-01-05 14:17:12 +0900

    Initialize literature-backend project with Gradle, Spring Boot, and Kotlin. Added essential configuration files, including .gitignore, .gitattributes, and build.gradle.kts. Implemented main application class and basic application properties for PostgreSQL integration. Included test setup for application context loading.

---
+++ src/test/kotlin/lab/ujumeonji/literaturebackend/LiteratureBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package lab.ujumeonji.literaturebackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LiteratureBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
