22e4bb75a9dd720b1f1130dfe9c5e0128d883a60
otter.moon <cd80@kakao.com>
2025-03-01 01:48:47 +0900

    Remove Spring Security starter from core module dependencies

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -24,7 +24,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.flywaydb:flyway-core")
     implementation("org.flywaydb:flyway-database-postgresql")
     implementation("com.github.f4b6a3:uuid-creator:6.0.0")
