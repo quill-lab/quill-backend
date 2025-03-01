67c4f3d8573b5ee5c6f6575cf8fd3c378eaa5e81
otter.moon <cd80@kakao.com>
2025-03-01 13:55:31 +0900

    Cleanup application properties and database migration
    Remove commented-out logging configurations and unnecessary table creation in migration script

--- api/src/main/resources/db/migration/V8__add_persisted_at_column.sql
+++ api/src/main/resources/db/migration/V8__add_persisted_at_column.sql
@@ -1,16 +1,3 @@
-create table chapter_metadata
-(
-    id            uuid not null,
-    created_at    timestamp without time zone,
-    persisted_at  timestamp without time zone,
-    updated_at    timestamp without time zone,
-    deleted_at    timestamp without time zone,
-    likes         integer,
-    views         integer,
-    comment_count integer,
-    constraint pk_chapter_metadata primary key (id)
-);
-
 alter table accounts
     add persisted_at timestamp without time zone;
 
