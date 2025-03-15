fc6ca030b039a0a5317b3a9eb3c06626713bfb07
otter.moon <cd80@kakao.com>
2025-03-15 21:47:20 +0900

    feat: Implement CheckEmailDuplicationUseCase for email validation
    - Added CheckEmailDuplicationUseCase class to handle email duplication checks.
    - Utilizes AccountService to verify if an email is already associated with an account.
    - Introduced Request and Response data classes for structured input and output.

---
+++ application/src/main/kotlin/lab/ujumeonji/literaturebackend/usecase/auth/CheckEmailDuplicationUseCase.kt
@@ -0,0 +1,28 @@
+package lab.ujumeonji.literaturebackend.usecase.auth
+
+import lab.ujumeonji.literaturebackend.domain.account.AccountService
+import lab.ujumeonji.literaturebackend.usecase.UseCase
+import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
+import java.time.LocalDateTime
+import java.util.*
+
+@Component
+@Transactional(readOnly = true)
+class CheckEmailDuplicationUseCase(
+    private val accountRepository: AccountService
+) : UseCase<CheckEmailDuplicationUseCase.Request, CheckEmailDuplicationUseCase.Response> {
+
+    override fun execute(request: Request, executedAt: LocalDateTime): Response {
+        val account = accountRepository.findByEmail(request.email)
+        return Response(isDuplicated = Objects.isNull(account))
+    }
+
+    data class Request(
+        val email: String
+    )
+
+    data class Response(
+        val isDuplicated: Boolean
+    )
+}
