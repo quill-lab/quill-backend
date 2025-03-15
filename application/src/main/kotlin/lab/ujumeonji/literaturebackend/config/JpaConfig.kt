e5c79db139fd16a9e9cce76729b9a1c59b37b095
otter.moon <cd80@kakao.com>
2025-03-15 18:44:13 +0900

    chore: Add Spring configuration annotation to JpaConfig for JPA auditing
    - Added @Configuration annotation to JpaConfig class to enable Spring's configuration capabilities.
    - This change enhances the setup for JPA auditing within the application.

--- application/src/main/kotlin/lab/ujumeonji/literaturebackend/config/JpaConfig.kt
+++ application/src/main/kotlin/lab/ujumeonji/literaturebackend/config/JpaConfig.kt
@@ -1,6 +1,8 @@
 package lab.ujumeonji.literaturebackend.config
 
+import org.springframework.context.annotation.Configuration
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @EnableJpaAuditing
+@Configuration
 class JpaConfig
